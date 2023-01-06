1b3423f09a5b8b079b2f11f828bbd70f6a9ee9b0
Nadya <119352923+Nadya-cheb@users.noreply.github.com>
2023-01-06 21:09:55 +0300

    Add files via upload
    docs: add js kod

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let sign = n > 0 ? 1 : -1;
+    let numString = '' + n*sign; //чтобы не использовать Math.abs
+    let res = '';
+    for (let i = numString.length - 1; i >= 0; i--) {
+        res = res + numString[i];
+    }
+    return Number(res);
 }
